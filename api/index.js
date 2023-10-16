c28262be7430efa1315073a23475dc35046814da
Michael Orecchia <michael.orecchia@gmail.com>
2023-10-16 09:24:30 -0700

    move/rename main and add headers

---
+++ api/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from 'cors';
+import { getList } from "../search.js";
+
+const app = express();
+const port = 3000;
+
+app.use(cors())
+
+app.get("/", async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-max-age=14400, stale-while-revalidate');
+  res.json('API working...');
+});
+
+app.get("/search", async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-max-age=60, stale-while-revalidate');
+  const term = req.query['term'];
+  const vids = term && await getList(term);
+  const filtered = vids && vids.filter(v => v.channelTitle);
+  res.json(filtered || []);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
