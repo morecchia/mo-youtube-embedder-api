1697f514ac5fc52700053053c53d4fb0685cf68b
Michael Orecchia <michael.orecchia@gmail.com>
2024-01-15 20:03:39 -0800

    add video description endpoint

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const search = require('./search.js');
-
 const app = express();
-
 const port = 3000;
 
 app.use(cors())
@@ -18,9 +16,16 @@ app.get("/search", async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Cache-Control', 's-max-age=60, stale-while-revalidate');
   const term = req.query['term'];
-  const vids = term && await search.getList(term);
-  const filtered = vids && vids.filter(v => v.channelTitle);
+  const filtered = term && await search.getList(term);
   res.json(filtered || []);
+});
+
+app.get("/description/:videoId", async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-max-age=60, stale-while-revalidate');
+  const { videoId } = req.params;
+  const video = await search.getDescription(videoId);
+  res.json(video);
 });
 
 app.listen(port, () => {
--- search.js
+++ search.js
@@ -1,8 +1,26 @@
 const youtube = require('youtube-search-api');
+const videoInfoName = 'videoSecondaryInfoRenderer';
+const descriptionKeyName = 'attributedDescription';
 
 async function getList(keywords) {
-    const list = await youtube.GetListByKeyword(keywords);
-    return list && list.items;
+  const list = await youtube.GetListByKeyword(keywords);
+  const items = list && list.items;
+  return items && items.filter(v => v.channelTitle)
 }
 
-module.exports = {getList};
+async function getDescription(videoId) {
+  if (!videoId) {
+    return null;
+  }
+  
+  const details = await youtube.GetVideoDetails(videoId);
+  const contents = details && details.contents;
+  const info = contents && contents.find(o => Object.keys(o).indexOf(videoInfoName) > -1)
+  const description = info && info[videoInfoName][descriptionKeyName];
+
+  return {
+    description: description && description.content
+  };
+}
+
+module.exports = {getList, getDescription};
